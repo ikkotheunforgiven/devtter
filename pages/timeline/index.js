8dc0923a2e7aa72865bcf43f626e781abee39d99
ikkotheunforgiven <ikkotheunforgiven@gmail.com>
2022-10-20 13:52:09 +0200

    Primeros pasos en Next.

---
+++ pages/timeline/index.js
@@ -0,0 +1,13 @@
+export default function Timeline() {
+    return (
+        <>
+            <h1>This is the timeline</h1>
+            <style jsx>{`
+                h1 {
+                    font-size: 36px;
+                    color: red;
+                }
+            `}</style>
+        </>
+    )
+}
