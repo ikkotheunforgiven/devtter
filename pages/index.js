7c0908b1f70d63f3a2f16a5fbba4983224e1de10
ikkotheunforgiven <ikkotheunforgiven@gmail.com>
2022-10-28 13:03:54 +0200

    Username fixed.

--- pages/index.js
+++ pages/index.js
@@ -46,7 +46,7 @@ export default function Home() {
             {
               user && user.avatar &&
               <span>
-                <Avatar alt={user.username} src={user.avatar} text={user.username} />
+                <Avatar alt={user.username} src={user.avatar} text={user.userName} />
               </span>
             }
         </span>
